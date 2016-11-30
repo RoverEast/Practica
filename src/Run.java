7d78edcab98277580adff2a120c784c39be3edda
Rovereast <karandashDG@gmail.com>
2016-11-30 05:37:39 +0300

    Add new version

--- src/Run.java
+++ src/Run.java
@@ -1,5 +1,7 @@
 public class Run {
     public static void main(String[] args) {
         System.out.println("Hello,java");
+        System.out.println("Hello,java");
+        System.out.println("Hello,java");
     }
 }
